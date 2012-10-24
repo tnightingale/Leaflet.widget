46b402962ca596221e3b4d6c3cd50c96dd8bb38f
Tom Nightingale <tom@affinitybridge.com>
2012-10-24 11:13:56 -0700

    fixed license

--- grunt.js
+++ grunt.js
@@ -10,8 +10,8 @@ module.exports = function(grunt) {
       banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
         '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>; */' //' +
-        // ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+        '* Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
     },
     lint: {
       files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
